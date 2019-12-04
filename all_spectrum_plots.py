a49d61e24ea1de56c21d430032f80f89003bf7f6
nastasimarco <marco.nastasi@gmail.com>
2019-12-04 15:58:22 +0100

    Fixed the figure size and added saving to png file.

--- all_spectrum_plots.py
+++ all_spectrum_plots.py
@@ -3,7 +3,10 @@
 import matplotlib.pyplot as plt
 import my_module
 
+plt.figure(figsize=(19, 11), dpi=100)
 for i in range(5):
     plt.subplot(3, 2, i+1)
     my_module.spectrum_plot('spettri_categorie.csv', i)
+plt.tight_layout()
+plt.savefig('all_spectrum.png')
 plt.show()
