f9c504a6e0057a49bfac012ff0c1351a46751d20
nastasimarco <marco.nastasi@gmail.com>
2019-12-04 11:07:39 +0100

    Minor changes

--- all_spectrum_plots.py
+++ all_spectrum_plots.py
@@ -1,7 +1,9 @@
+""" Make all spectrum plot needed, each in a subplot.
+"""
 import matplotlib.pyplot as plt
 import my_module
 
 for i in range(5):
-    plt.subplot(3,2,i+1)
+    plt.subplot(3, 2, i+1)
     my_module.spectrum_plot('spettri_categorie.csv', i)
 plt.show()
--- my_module.py
+++ my_module.py
@@ -1,24 +1,33 @@
+"""
+This module contains functions that I found useful.
+For now it's generic. Later it would be better to divide the functions
+in module with some shared context. For now this will serve.
+
+List of functions:
+- spectrum_plot(inputfile, category=0, xticks_step=3)
+
+Author: Marco Nastasi
+"""
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 def spectrum_plot(inputfile, category=0, xticks_step=3):
-    """
-    This function reads the inputfile of leq and spectrum
-    and gives a bar plot with errorbars of the selected category.
-    - *inputfile* is the .csv file containing the database.
-    - *category* (default=0) is an index used to select the 
-    category corresponding to the chosen column
+    """Read the inputfile of leq and spectrum and return a bar plot with
+    errorbars of the selected category.
+    - inputfile -- is the .csv file containing the database
+    - category -- is an index used to select the category corresponding
+    to the chosen column (default 0)
     (es: for Traghetto category input category=4).
-    - *xticks_step* (default=3) select the step of the ticks in the
-    x axis of the plot.
-    
+    - xticks_step -- select the step of the ticks in the x axis of the
+    plot (default=3)
     Requires: pandas, numpy, matplotlib.
     """
     data = pd.read_csv(inputfile,
-                        sep=';',
-                        header=[0,1], # column MultiIndex with first 2 rows
-                        index_col=[0]) # first column as index
+                       sep=';',
+                       header=[0, 1], # column MultiIndex with first 2 rows
+                       index_col=[0]) # first column as index
 
     # renaming column MultiIndex
     new_columns = []
@@ -36,10 +45,10 @@ def spectrum_plot(inputfile, category=0, xticks_step=3):
 
     # choosing a column (category) and preparing data into np.arrays
     j_col = category # used on the reduced DataFrame even_col and odd_col
-    chosen_col = even_col[even_col.columns[j_col]] # 1 column Pandas DataFrame
+    chosen_col = even_col[even_col.columns[j_col]] # 1 column pandas DataFrame
     x_data = np.array(chosen_col.index)
     y_data = chosen_col.values
-    y_errors = odd_col.values[:,j_col]
+    y_errors = odd_col.values[:, j_col]
 
     # making a plot of chosen column (category)
     # plt.figure()
@@ -49,13 +58,13 @@ def spectrum_plot(inputfile, category=0, xticks_step=3):
     #            ('1/3 octave bands', '$L_{Aeq}$ (dB(A))'))
     plt.legend((spectrum_plt, leq_plt),
                ('1/3 octave bands', 'broadband noise (dB(A))'))
-    plot_title = f'Sound pressure spectrum - category: {chosen_col.name[0]}' 
+    plot_title = f'Sound pressure spectrum - category: {chosen_col.name[0]}'
     plt.title(plot_title)
     plt.xlabel('Frequency bands (Hz)')
     plt.ylabel('Sound pressure level (dB)')
     # selecting a subset of ticks for x axes of the plot
     new_xticks = np.arange(0, len(even_col.index)-1, step=xticks_step)
-    new_ticks_labels = [even_col.index[new_xticks][i].replace("Hz","")
-                            for i in range(len(new_xticks))]
+    new_ticks_labels = [even_col.index[new_xticks][i].replace("Hz", "")
+                        for i in range(len(new_xticks))]
     plt.xticks(new_xticks, new_ticks_labels)
     # plt.show()
