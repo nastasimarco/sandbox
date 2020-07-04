d9c558c740c125aa4a88d72d1c5d69e5bdf7b172
nastasimarco <55702553+nastasimarco@users.noreply.github.com>
2020-07-04 21:10:47 +0200

    First package attempt.

---
+++ sandbox/main_code.py
@@ -0,0 +1,19 @@
+"""Core part of the sandbox package. Here I try whatever comes to mind.
+"""
+# import sys
+import numpy as np
+
+def simple_print():
+    """Dummy function
+
+    Prints an array
+
+    Returns:
+        None
+    """
+    my_array = np.arange(10)
+    print(my_array)
+    # return None
+
+if __name__ == "__main__":
+    simple_print()
